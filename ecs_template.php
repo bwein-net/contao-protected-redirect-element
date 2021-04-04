4b38a052ad9fef8076c4a2a417b4e52ac2d95c72
Ralf Baussenwein <ralf@bwein.net>
2021-04-04 14:29:52 +0200

    Update ecs config

--- ecs_template.php
+++ ecs_template.php
@@ -6,14 +6,11 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $vendorDir = __DIR__.'/vendor';
+    $vendorDir = __DIR__ . '/vendor';
 
     if (!is_dir($vendorDir)) {
-        $vendorDir = __DIR__.'/../../vendor';
+        $vendorDir = __DIR__ . '/../../vendor';
     }
 
-    $containerConfigurator->import($vendorDir.'/contao/easy-coding-standard/config/template.php');
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_template_cache_protected_redirect_element');
+    $containerConfigurator->import($vendorDir . '/contao/easy-coding-standard/config/template.php');
 };
