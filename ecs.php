99a630b7bfbcd885467b03fdde7074b30007dd8f
Ralf Baussenwein <ralf@bwein.net>
2026-07-25 15:41:07 +0200

    Update ecs.php to new EasyCodingStandard config API

--- ecs.php
+++ ecs.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
+return ECSConfig::configure()
+    ->withConfiguredRule(HeaderCommentFixer::class, [
         'header' => "This file is part of Protected Redirect Element for Contao Open Source CMS.\n\n(c) bwein.net\n\n@license MIT",
     ]);
-};
